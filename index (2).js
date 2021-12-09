f21e2f3bba7723c938c1db34be2b1299c99049bd
ibrahim omileye <omileyeibrahim1992@gmail.com>
2021-12-09 04:30:56 +0100

    bro ibroski44

---
+++ index (2).js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+var firstname = "john";
+var lastname = "ibrahim";
+// printing values
+document.write(firstname + lastname);
+<script>
+var x = 21;
+document.write(x); // prints the value of x
+</script>
+
+var y = 3;
---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".container").style.color = "gold";
+document.querySelector("h1").style.fontSize = "10rem"
+//document.querySelector("h1").innerHTML = "<em>brown";
+document.querySelectorAll("a .nav-link")[1].style.color = "black";
